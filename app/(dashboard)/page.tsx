db919249c82994a9fd5d38aafd6b50f917492fc0
Ahmed Zeyad <keyboord01@gmail.com>
2024-06-24 01:34:26 +0300

    chore: Add account creation functionality and UI components and sheet provider

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+
 export default function Home() {
-  return <div>homepage</div>;
+  const { onOpen } = useNewAccount();
+
+  return (
+    <div>
+      <Button onClick={onOpen} className="bg-blue-500 text-white">
+        add an account
+      </Button>
+    </div>
+  );
 }
