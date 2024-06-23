923f80bd2bdc5f8486fd339a1edfc8bee2b7ad49
Ahmed Zeyad <keyboord01@gmail.com>
2024-06-23 09:51:54 +0300

    Authentication routes and logic

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,29 @@
+import { Loader2 } from "lucide-react";
+import { SignIn, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
+      <div className="h-full lg:flex flex-col items-center justify-center px-4">
+        <div className="text-center space-y-4 pt-16">
+          <h1 className="font-bold text-3xl text-slate-900">Welcome Back!</h1>
+          <p className="text-base text-slate-500">
+            Log in to your account to continue using our services.
+          </p>
+        </div>
+        <div className="flex  items-center justify-center mt-8">
+          <ClerkLoading>
+            <Loader2 size={32} className="animate-spin text-muted-foreground" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignIn />
+          </ClerkLoaded>
+        </div>
+      </div>
+      <div className="h-full bg-pink-600 hidden lg:flex items-center justify-center">
+        <Image src="/logo.svg" height={100} width={100} alt="logo" />
+      </div>
+    </div>
+  );
+}
