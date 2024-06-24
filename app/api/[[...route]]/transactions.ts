4fd18a830d8dfa2710302ce84a394358d47cfb3e
Ahmed Zeyad <keyboord01@gmail.com>
2024-06-24 19:42:41 +0300

    chore: Implement useGetTransaction, useGetTransactions, useCreateTransaction, useDeleteTransaction, and useBulkCreateTransactions hooks

--- app/api/[[...route]]/transactions.ts
+++ app/api/[[...route]]/transactions.ts
@@ -142,6 +142,35 @@ const app = new Hono()
     }
   )
   .post(
+    "/bulk-create",
+    clerkMiddleware(),
+    zValidator("json", z.array(insertTransactionSchema.omit({ id: true }))),
+    async (c) => {
+      const auth = getAuth(c);
+      const values = c.req.valid("json");
+
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      if (values.length === 0) {
+        return c.json({ error: "Invalid request" }, 400);
+      }
+
+      const data = await db
+        .insert(transactions)
+        .values(
+          values.map((value) => ({
+            id: createId(),
+            ...value,
+          }))
+        )
+        .returning();
+
+      return c.json({ data });
+    }
+  )
+  .post(
     "/bulk-delete",
     clerkMiddleware(),
     zValidator(
