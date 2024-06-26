0d782cdd036d7589d77af7ccce8bb5a0720cbbf8
Ahmed Zeyad <keyboord01@gmail.com>
2024-06-26 09:43:41 +0300

    feat: Add for performance optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 import { QueryProvider } from "@/providers/query-provider";
