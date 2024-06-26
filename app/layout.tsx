103a9813659f964fe62e7fa83601f78af9196696
Ahmed Zeyad <keyboord01@gmail.com>
2024-06-26 09:53:10 +0300

    Adding vercel speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <SpeedInsights />
           <QueryProvider>
             <SheetProvider />
             <Toaster />
